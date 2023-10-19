4d227dbe9bce551aa58de66c158d488201c96c24
Stefan Koev <st.koev@outlook.com>
2023-10-19 21:53:48 +0300

    build app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React pizza Co.</Link>
+      <p>Stefan</p>
+    </header>
+  );
+}
+
+export default Header;
