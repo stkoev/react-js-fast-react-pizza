66ee903be875df298fc69556cf9220cd2f973037
Stefan Koev <st.koev@outlook.com>
2023-10-20 14:30:15 +0300

    implement fetch order

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React pizza Co.</Link>
+      <SearchOrder />
       <p>Stefan</p>
     </header>
   );
