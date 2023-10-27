98e98805cdb3c18d7f617a8b063ccdb081ea59cc
Stefan Koev <st.koev@outlook.com>
2023-10-27 11:13:56 +0300

    style form elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SearchOrder() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   function handleChange(e) {
-    if (e.target.value === "") return;
+    if (e.target.value === '') return;
     setQuery(e.target.value);
   }
 
@@ -14,7 +14,7 @@ function SearchOrder() {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery("");
+    setQuery('');
   }
 
   return (
@@ -23,6 +23,7 @@ function SearchOrder() {
         placeholder="Search order"
         value={query}
         onChange={handleChange}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       ></input>
     </form>
   );
